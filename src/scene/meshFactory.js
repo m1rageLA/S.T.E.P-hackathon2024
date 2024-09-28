830d4bd51d412aa23831772d2aefd983826b8323
Maksim23123 <102969640+Maksim23123@users.noreply.github.com>
2024-09-28 18:32:33 +0200

    Load models to 3d scene

--- src/scene/meshFactory.js
+++ src/scene/meshFactory.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import { createMesh } from './meshCreator.js'
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 function createMeshes(scene) {
   const characterMesh = createMesh({
@@ -8,34 +9,6 @@ function createMeshes(scene) {
   });
   scene.add(characterMesh);
 
-  const terminalMesh = createMesh({
-    size: {x: 1, y: 1, z: 1},
-    position: {x: 10, y: 0, z: 0},
-    material: new THREE.MeshStandardMaterial({ color: 0x304fff }),
-  });
-  scene.add(terminalMesh);
-
-  const computerMesh = createMesh({
-    size: {x: 1, y: 1, z: 1},
-    position: {x: -10, y: 0, z: 0},
-    material: new THREE.MeshStandardMaterial({ color: 0x304fff }),
-  });
-  scene.add(computerMesh);
-
-  const testMesh = createMesh({
-    size: {x: 1, y: 1, z: 1},
-    position: {x: 0, y: 0, z: -7},
-    material: new THREE.MeshStandardMaterial({ color: 0x304fff }),
-  });
-  scene.add(testMesh);
-
-  const obstacle1Mesh = createMesh({
-    size: {x: 3, y: 3, z: 6},
-    position: {x: 2, y: 1, z: -3},
-    material: new THREE.MeshStandardMaterial({ color: 0x304fff }),
-  });
-  scene.add(obstacle1Mesh);
-
   const platformMesh = createMesh({
     size: {x: 500, y: 1, z: 500},
     position: {x: 0, y: -1, z: 0},
@@ -43,15 +16,57 @@ function createMeshes(scene) {
   });
   scene.add(platformMesh);
 
+
+  // Load scene model
+  const obstacles = [];
+
+  const loader = new GLTFLoader();
+  loader.load(
+    "/SceneNotComplete.glb",
+    (gltf) => {
+      const DEG2RAD = Math.PI / 180;
+
+      // Setup scene model on scene
+      const sceneModel = gltf.scene;
+      sceneModel.position.set(2, -0.5, 2);
+      const sceneScale = 0.7;
+      sceneModel.scale.set(sceneScale, sceneScale, sceneScale);
+      sceneModel.rotation.set(0, -90 * DEG2RAD, 0)
+      sceneModel.castShadow = true;
+
+      // Search for obstacles in scene
+      const obstacleTresholdSize = 1;
+      sceneModel.traverse((child) => {
+        if (child.isMesh) {
+          child.castShadow = true;
+          child.receiveShadow = true;
+          child.metalness = 0;
+
+          const box = new THREE.Box3().setFromObject(child);
+
+          const size = new THREE.Vector3();
+          box.getSize(size);
+
+          const sizeList = [...size];
+
+          if (sizeList.some(item => item >= obstacleTresholdSize)) {
+            obstacles.push(child);
+          }
+        }
+      });
+
+      scene.add(sceneModel);
+    },
+    undefined,
+    (error) => {
+      console.error("An error occurred while loading the model:", error);
+    }
+  );
+
   // Return an object containing the meshes for later access
   return {
-    obstacles: {
-      obstacle1Mesh
-    },
+    obstacles,
     characterMesh,
-    terminalMesh,
-    computerMesh,
-    testMesh,
     platformMesh
   };
 }
