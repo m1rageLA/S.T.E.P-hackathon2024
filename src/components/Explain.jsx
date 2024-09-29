cb514f9a01b0bbac5b8685b59e4020905aa8e5bc
Maksym Boiko <maksym782@gmail.com>
2024-09-29 11:47:37 +0200

    added logic

--- src/components/Explain.jsx
+++ src/components/Explain.jsx
@@ -1,10 +1,39 @@
 import Button from "./Button"
+import { useState } from "react";
 const Explain = () => {
+  const [sentence, setSentence] = useState("")
+  const [result, setResult] = useState("")
+
+  const handleGetMark = async () => {
+
+      const packageData = localStorage.getItem('packageData')
+      if(!packageData)
+        return
+      const response = await fetch("http://localhost:5062/api/AICommunication/generate-teach-mark", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ 
+          "topicName": packageData.topicName,
+          "material": packageData.material,
+          "sentence": sentence })
+    });
+
+    if (!response.ok) {
+        throw new Error("Login failed. Please check your credentials.");
+    }
+
+    const data = await response.json();
+    setResult(data.mark.toString())
+    
+  }
   return (
     <div className="terminal">
       <div className="terminal__window">
         <div className="terminal-line">
           <h3>Explain the topic:</h3>
+          <p>{result??""}</p>
           <a href="">
             <img
               src="close.png"
@@ -17,9 +46,11 @@ const Explain = () => {
           id="theoryField"
           rows="10"
           cols="50"
+          value={sentence} 
+          onChange={(e) => setSentence(e.target.value)} 
           placeholder="Tell AI what you learned in one sentence."
         ></textarea>
-        <Button type={"submit"} value={"Send"}/>
+        <Button type={"submit"} value={"Send"} onClick={(e)=>handleGetMark()}/>
       </div>
     </div>
   );
